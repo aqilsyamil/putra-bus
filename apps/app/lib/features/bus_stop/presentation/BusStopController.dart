31a050156aab261cec3c238b0387ec6b6bd2b282
rayhanasyraff <rayhanasyraffhjamran@gmail.com>
2024-03-25 15:37:30 +0800

    List all bus stops data from csv

--- apps/app/lib/features/bus_stop/presentation/BusStopController.dart
+++ apps/app/lib/features/bus_stop/presentation/BusStopController.dart
@@ -3,8 +3,7 @@ import 'package:app/features/bus_stop/domain/BusStop.dart';
 import 'package:app/features/bus_stop/data/BusStopRepository.dart';
 
 class BusStopController {
-  List<BusStop> getBusStops() {
+  Future<List<BusStop>> getBusStops() {
     return BusStopRepository.getBusStops();
   }
-
 }
--- apps/app/lib/features/bus_stop/presentation/BusStopWidget.dart
+++ apps/app/lib/features/bus_stop/presentation/BusStopWidget.dart
@@ -2,16 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:app/features/bus_stop/domain/BusStop.dart';
 import 'package:app/features/bus_stop/presentation/BusStopController.dart';
 
-class BusStopWidget extends StatelessWidget {
+class BusStopWidget extends StatefulWidget {
+  const BusStopWidget({super.key});
+
+  @override
+  _BusStopWidgetState createState() => _BusStopWidgetState();
+}
+
+class _BusStopWidgetState extends State<BusStopWidget> {
+  final List<BusStop> busStops = [];
   final BusStopController _busStopController = BusStopController();
 
   @override
-  Widget build(BuildContext context) {
-    final List<BusStop> busStops = _busStopController.getBusStops();
+  void initState() {
+    super.initState();
+    BusStopController().getBusStops().then((value) {
+      setState(() {
+        busStops.addAll(value);
+      });
+    });
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Bus Stops App',
-      debugShowCheckedModeBanner: false, 
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -19,19 +35,21 @@ class BusStopWidget extends StatelessWidget {
         appBar: AppBar(
           title: Text('Bus Stops'),
         ),
-        body: ListView.builder(
-          itemCount: busStops.length,
-          itemBuilder: (context, index) {
-            final busStop = busStops[index];
-            return ListTile(
-              title: Text(busStop.longName),
-              subtitle: Text(busStop.shortName),
-              onTap: () {
-                // You can perform actions when a bus stop is tapped
-              },
-            );
-          },
-        ),
+        body: busStops.isEmpty
+            ? Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: busStops.length,
+                itemBuilder: (context, index) {
+                  final busStop = busStops[index];
+                  return ListTile(
+                    title: Text(busStop.longName),
+                    subtitle: Text(busStop.shortName),
+                    onTap: () {
+                      // You can perform actions when a bus stop is tapped
+                    },
+                  );
+                },
+              ),
       ),
     );
   }
