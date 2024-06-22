a6c068f696d1db032861855522e856234c8a4547
Aqil <aqilsyamil8@gmail.com>
2024-06-22 22:14:20 +0800

    Fix navigation bar for iphone view and add star icon for favourite for bus stop page

--- apps/app/components/bus-stop/BusStopItem.tsx
+++ apps/app/components/bus-stop/BusStopItem.tsx
@@ -1,5 +1,6 @@
 import { BusStop } from "@/types/data";
 import { View, Text } from "react-native";
+import StarIcon from "@/icons/StarIcon";
 
 interface BusStopItemProps {
   busStop: BusStop;
@@ -10,12 +11,17 @@ export default function BusStopItem({ busStop }: BusStopItemProps): JSX.Element 
   console.log(busStop);
 
   return (
-    <View className="m-5">
-      <Text className="text-xl">{busStop.fullName}</Text>
-      <View className="flex flex-row gap-1">
-        <Text className="text-grey-500">{busStop.shortName}</Text>
-        <Text className="text-grey-500">•</Text>
-        <Text className="text-grey-500">SJ155</Text>
+    <View className="flex flex-col py-4 px-2 border-b border-slate-200">
+      <View className="flex flex-row items-center">
+        <StarIcon color="#333231" />
+        <View className="flex flex-col">
+          <Text className="text-base text-black font-semibold px-2">{busStop.fullName}</Text>
+          <View className="flex flex-row items-center">
+            <Text className="text-slate-600 font-semibold px-2">{busStop.shortName}</Text>
+            <Text className="text-slate-600 ml-1">•</Text>
+            <Text className="text-slate-600 font-semibold ml-1">SJ155</Text>
+          </View>
+        </View>
       </View>
     </View>
   );
